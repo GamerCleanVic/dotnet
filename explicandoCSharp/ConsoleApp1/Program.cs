a8f462a6327e6fd6118e374d1cee0b00ed6af467
GamerCleanVic <jonassd2000@gmail.com>
2024-08-24 19:17:50 -0300

    Soma em CSharp

--- explicandoCSharp/ConsoleApp1/Program.cs
+++ explicandoCSharp/ConsoleApp1/Program.cs
@@ -4,7 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Digite nºs a serem somados.");
+
+        Console.WriteLine("1º nº: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("2º nº: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Soma = {Soma(num1, num2)}");
+
+        Console.WriteLine("Aperte Enter para finalizar o programa.");
+
+        Console.ReadLine();
     }
 
     public static int Soma(int num1, int num2){
