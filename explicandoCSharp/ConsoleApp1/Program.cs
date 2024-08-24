2a8d8380374f9f9dfd16dc0af7f14a569923c647
GamerCleanVic <jonassd2000@gmail.com>
2024-08-24 18:03:04 -0300

    first commit

---
+++ explicandoCSharp/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static int Soma(int num1, int num2){
+        return num1 + num2;
+    }
+}
