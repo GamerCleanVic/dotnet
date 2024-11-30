44b6ee9914e5764f9480dad5aea9dbcc166fe270
GamerCleanVic <jonassd2000@gmail.com>
2024-11-30 15:54:42 -0300

    namespace

---
+++ AulasNameSpace/ConsoleNameSpace/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ConsoleNameSpace{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello, World!");
+
+            Carro carro = new Carro();
+            carro.DigaSeuNome();
+            Aviao aviao = new Aviao();
+            aviao.DigaSeuNome();
+            MeusTestes.Carro carro1 = new MeusTestes.Carro();
+            carro1.DigaSeuNome();
+
+            Console.Read();
+        }
+    }
+
+    class Carro{
+        public void DigaSeuNome(){
+            Console.WriteLine("Eu sou um carro");
+        }
+    }
+}
+
+namespace MeusTestes{
+    class Carro{
+        public void DigaSeuNome(){
+            Console.WriteLine("Eu sou um carro dentro do MeusTestes");
+        }
+    }
+}
+class Aviao{
+    public void DigaSeuNome(){
+        Console.WriteLine("Eu sou um aviao");
+    }
+}
--- explicandoCSharp/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ explicandoCSharp/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc65c5f55f5f16d123f6b6ab80f3bb2937f9dbe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf38f03b40ff7dda838088eff1499ebf85d524a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
